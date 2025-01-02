01c8f842c4aa9f0b97449a5b3ab29167ac5bb53a
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2025-01-03 00:42:29 +0300

    Add files via upload

---
+++ files/js/index.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inp = document.querySelector("#check");
+    const panel = document.querySelector(".panel");
+
+    const tl = gsap.timeline({ paused: true });
+    gsap.to(panel, {
+        xPercent: 100,
+        duration: .001
+    })
+    inp.addEventListener("change", () => {
+        if (inp.checked) {
+            tl.clear();
+            tl.to(panel, {
+                xPercent: 0,
+                duration: 0.7,
+                ease: "power2.out",
+                onStart: () => {
+                    panel.style.display = "flex";
+                },
+            });
+        } else {
+            tl.clear();
+            tl.to(panel, {
+                duration: 0.7,
+                xPercent: 100,
+                ease: "power2.out",
+                onComplete: () => {
+                    panel.style.display = "none";
+                },
+            });
+        }
+        tl.play();
+    });
+});
+
+//заткмнение
+// if (inp.checked) {
+//     tl.clear();
+//     tl.to(panel, {
+//         opacity: 1,
+//         duration: 0.8,
+//         onStart: () => {
+//             panel.style.display = "flex";
+//         },
+//     });
+// } else {
+//     tl.clear();
+//     tl.to(panel, {
+//         opacity: 0,
+//         duration: 0.8,
+//         onComplete: () => {
+//             panel.style.display = "none";
+//         },
+//     });
+// }
+// tl.play();
